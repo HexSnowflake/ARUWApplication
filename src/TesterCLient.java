cf3546fe370933f876a9422cd3328f75113b7d05
ApCsUses <s-daik@bsd405.org>
2021-10-03 00:20:00 -0700

    add comprehensive test to client

--- src/TesterCLient.java
+++ src/TesterCLient.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class TesterCLient {
     public static void main(String[] args) {
         int[] WVM = new int[] {1, 2, 3,};
-        int[] VTM = new int[] {2, 3, 1, 2, 3, 4, 5, 6};
+        int[] VTM = new int[] {2, 3, 1, 2, 3, 4, 5, 1};
+        int[] comprehensiveTest = new int[] {1, 5, 6, 2, 2, 1, 2, 1, 4, 1, 3, 2};
 
 
         ProtocolParser tester = new ProtocolParser();
@@ -13,10 +15,18 @@ public class TesterCLient {
         }
         System.out.println(tester.processNewWord(WVM[WVM.length - 1]));
 
-        //testVTMprocessing
+        //test VTMprocessing
         for(int i = 0; i < VTM.length - 1; i++) {
             tester.processNewWord(VTM[i]);
         }
         System.out.println(tester.processNewWord(VTM[VTM.length - 1]));
+
+        //test comprehensiveTest
+        for(int i = 0; i < comprehensiveTest.length; i++) {
+            ArrayList<String> result = tester.processNewWord(comprehensiveTest[i]);
+            if(i == 2 || i == 8 || i == comprehensiveTest.length - 1) {
+                System.out.println(result);
+            }
+        }
     }
 }
